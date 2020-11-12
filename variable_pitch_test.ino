ef53d0aa499407b1e1e71e18c5a5c018803d9e34
OkitaTomoya <special1168@gmail.com>
2020-11-12 15:26:29 +0900

    use pwm shield, push from 201112 dynabook

--- variable_pitch_test.ino
+++ variable_pitch_test.ino
@@ -1,7 +1,16 @@
-#include <Arduino.h>
-#include <Servo.h>
-int volume1_val = 0;
-int volume2_val = 0;
+#include <Wire.h>
+#include "my_Adafruit_PWMServoDriver.h"
+#define SERVO_FREQ 200
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+//------lateral_rotor-----
+//lateral brushless motor pin
+const int lateral_brushless_shiledpin = 1;
+//lateral servo motor pin
+const int lateral_servo1_shieldpin = 2;
+const int lateral_servo2_shieldpin = 3;
+const int lateral_servo3_shieldpin = 4;
+
 float throttle = 0.0;
 int throttle_command = 0;
 float first_pitch = 330.0;
@@ -9,12 +18,6 @@ float pitch = first_pitch;
 int servo1_command = 1500;
 int servo2_command = 1500;
 int servo3_command = 1500;
-Servo servo1;
-Servo servo2;
-Servo servo3;
-Servo brushlessmotor;
-float rc = 0.0;
-float a = 0.95;
 
 int limit_servo_command_value(float value)
 {
@@ -37,29 +40,20 @@ int limit_servo_command_value(float value)
 void setup()
 {
   Serial.begin(115200);
-  servo1.attach(2);
-  servo2.attach(3);
-  servo3.attach(4);
-  brushlessmotor.attach(5);
 
   servo1_command = limit_servo_command_value(2000.0 - first_pitch);
   servo2_command = limit_servo_command_value(1000.0 + first_pitch);
   servo3_command = limit_servo_command_value(1000.0 + first_pitch);
 
-  brushlessmotor.writeMicroseconds(1000);
-  servo1.writeMicroseconds(servo1_command);
-  servo2.writeMicroseconds(servo2_command);
-  servo3.writeMicroseconds(servo3_command);
+  pwm.writeMicroseconds(lateral_brushless_shiledpin, 1000.0);
+  pwm.writeMicroseconds(lateral_servo1_shieldpin, servo1_command);
+  pwm.writeMicroseconds(lateral_servo2_shieldpin, servo2_command);
+  pwm.writeMicroseconds(lateral_servo3_shieldpin, servo3_command);
   delay(8000);
 }
 
 void loop()
 {
-  //  volume1_val = analogRead(A0);
-  //  volume2_val = analogRead(A1);
-  //  throttle = a * throttle + (1.0 - a) * (float)volume1_val;
-  //  pitch = a * pitch + (1.0 - a) * (float)volume2_val;
-
   if (Serial.available())
   {
     char key;            // 受信データがあるか？
@@ -77,8 +71,8 @@ void loop()
   servo1_command = limit_servo_command_value(2000.0 - pitch);
   servo2_command = limit_servo_command_value(1000.0 + pitch);
   servo3_command = limit_servo_command_value(1000.0 + pitch);
-  throttle = 800.0;
-  throttle_command = limit_servo_command_value(1000.0 + throttle);
+  throttle = 1800.0;
+  throttle_command = limit_servo_command_value(throttle);
 
   Serial.print("pitch=");
   Serial.print(pitch);
@@ -89,9 +83,9 @@ void loop()
   Serial.print(',');
   Serial.println(servo3_command);
 
-  servo1.writeMicroseconds(servo1_command);
-  servo2.writeMicroseconds(servo2_command);
-  servo3.writeMicroseconds(servo3_command);
-  brushlessmotor.writeMicroseconds(throttle_command);
+  pwm.writeMicroseconds(lateral_brushless_shiledpin, throttle_command);
+  pwm.writeMicroseconds(lateral_servo1_shieldpin, servo1_command);
+  pwm.writeMicroseconds(lateral_servo2_shieldpin, servo2_command);
+  pwm.writeMicroseconds(lateral_servo3_shieldpin, servo3_command);
   delay(10);
 }
